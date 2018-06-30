fa87b80594f8265033de8227de15080b91a152bc
vasilii <vasilii_tolkachev@epam.com>
2018-06-30 16:11:10 +0300

    add users

---
+++ src/main/java/sia/tacocloud/SecurityConfig.java
@@ -0,0 +1,32 @@
+package sia.tacocloud;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("USER", "ADMIN");
+    }
+}
--- src/main/java/sia/tacocloud/TacoCloudApplication.java
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -2,10 +2,12 @@ package sia.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("sia.tacocloud.data")
+@Import(SecurityConfig.class)
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
